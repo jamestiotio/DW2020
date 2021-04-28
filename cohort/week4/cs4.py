082389cce5e97baa53057fc32a32fbc316e891a7
James R T <jamestiotio@gmail.com>
2021-04-28 19:47:55 +0800

    Format tuple as list

--- cohort/week4/cs4.py
+++ cohort/week4/cs4.py
@@ -1,5 +1,5 @@
 def transpose_matrix(matrix):
-    # return list(zip(*matrix))
+    # return [list(i) for i in zip(*matrix)]
     new_matrix = [[] for i in range(len(matrix[0]))]
     for row in matrix:
         for index, element in enumerate(row):
