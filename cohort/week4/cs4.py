6813d76c4e891521b4f1d448959f6ae84478562a
James R T <jamestiotio@gmail.com>
2021-04-28 19:46:03 +0800

    Add another possible method to transpose matrix

--- cohort/week4/cs4.py
+++ cohort/week4/cs4.py
@@ -1,4 +1,5 @@
 def transpose_matrix(matrix):
+    # return list(zip(*matrix))
     new_matrix = [[] for i in range(len(matrix[0]))]
     for row in matrix:
         for index, element in enumerate(row):
